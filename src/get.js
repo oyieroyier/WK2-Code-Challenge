9416c58d6e7f14a5be6695e89fa88432038dc4df
Bob Oyier <boboyier92@gmail.com>
2022-12-05 20:49:55 +0300

    "preliminary submission WK2 Code Challenge"

--- src/get.js
+++ src/get.js
@@ -1,1 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => console.log("DOM has loaded"))
+// Your code here
+const character = document.getElementById("character-bar");
+// const container = document.getElementById("detailed-info");
+const animalName = document.getElementById("name");
+const image = document.getElementById("image");
+const form = document.getElementById("votes-form");
+const animalVotes = document.getElementById("vote-count");
+const input = document.getElementById("votes");
+const resetVotes = document.getElementById("reset-btn");
+let currentAnimal;
+resetVotes.style.cursor = "pointer";
+resetVotes.style.fontFamily = "'Roboto Mono', monospace";
+
+// endpoint for fetch data
+function getCharacters() {
+	fetch("http://localhost:3000/characters/")
+		.then((response) => response.json())
+		.then(renderAnimals);
+}
+// function that accesses the payload
+function renderAnimals(animals) {
+	animals.forEach(renderCharacters);
+}
+
+// function that accesses individual payload elements and show their required data
+function renderCharacters(animal) {
+	const spanEle = document.createElement("span");
+	spanEle.innerHTML = animal.name;
+	spanEle.style.cursor = "pointer";
+	character.appendChild(spanEle);
+	spanEle.addEventListener("click", () => {
+		currentAnimal = animal;
+		showAnimal(animal);
+	});
+}
+
+// A function that dsplays curated data about the animal in question
+function showAnimal(animal) {
+	animalName.innerHTML = animal.name;
+	image.src = animal.image;
+	animalVotes.innerHTML = animal.votes;
+}
+getCharacters();
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	currentAnimal.votes += parseInt(e.target.votes.value);
+	showAnimal(currentAnimal);
+	form.reset();
+});
+
+resetVotes.addEventListener("click", () => {
+	currentAnimal.votes = 0;
+	showAnimal(currentAnimal);
+});
